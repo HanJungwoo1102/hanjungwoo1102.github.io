4968aeca3980387d501928b9e8b4f86d6d874f97
HanJungwoo <ajtwlswjddnv1102@waug.com>
2020-03-12 15:32:29 +0900

    feat:add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,5 +80,11 @@ module.exports = {
         {
             resolve: `gatsby-plugin-styled-components`,
         },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-118250676-2",
+            },
+        },
     ]
 };
